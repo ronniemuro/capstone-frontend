ed1c88131684435a0e2dc5e3768c116a3d346e24
Ronnie Lopez-Muro <ronniethan@outlook.com>
2022-07-01 20:42:24 +0200

    minor change to index with user image added to post

--- src/views/PostsIndex.vue
+++ src/views/PostsIndex.vue
@@ -24,7 +24,10 @@ export default {
 <template>
   <div class="home">
     <div v-for="post in posts" v-bind:key="post.id">
-      <h4>{{ post.user.name }} @{{ post.user.username }}</h4>
+      <h4>
+        <img v-bind:src="post.user.profile_pic" v-bind:alt="post.user.name" />
+        {{ post.user.name }} @{{ post.user.username }}
+      </h4>
       <h3>{{ post.post_content }}</h3>
       <h3>{{ post.sign_type }}: {{ post.sign }}</h3>
     </div>
