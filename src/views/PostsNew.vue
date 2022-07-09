6d0daeae6d9ccd127a377214bf7cdc09d614844c
Ronnie Lopez-Muro <ronniethan@outlook.com>
2022-07-09 18:53:51 +0200

    adds styling to new post page

--- src/views/PostsNew.vue
+++ src/views/PostsNew.vue
@@ -29,43 +29,126 @@ export default {
 
 <template>
   <div class="new-post">
-    <form v-on:submit.prevent="submit()">
-      <h2>New Post</h2>
-      <ul>
-        <li v-for="error in errors" v-bind:key="error">{{ error }}</li>
-      </ul>
-      <div>
-        <label>How are the planets aligning for you?</label>
-        <input type="text" v-model="newPostParams.post_content" />
-      </div>
-      <div>
-        <label>Sign Type:</label>
-        <select name="Sign" v-model="newPostParams.sign_type">
-          <option value="Sun">Sun</option>
-          <option value="Moon">Moon</option>
-          <option value="Rising">Rising</option>
-        </select>
-      </div>
-      <div class="dropdown">
-        <label>Sign:</label>
-        <select name="Sign" v-model="newPostParams.sign">
-          <option value="Aries">Aries</option>
-          <option value="Taurus">Taurus</option>
-          <option value="Gemini">Gemini</option>
-          <option value="Cancer">Cancer</option>
-          <option value="Leo">Leo</option>
-          <option value="Virgo">Virgo</option>
-          <option value="Libra">Libra</option>
-          <option value="Scorpio">Scorpio</option>
-          <option value="Sagittarius">Sagittarius</option>
-          <option value="Capricorn">Capricorn</option>
-          <option value="Aquarius">Aquarius</option>
-          <option value="Pisces">Pisces</option>
-        </select>
-      </div>
-      <input type="submit" value="Create" />
-    </form>
+    <main id="main">
+      <section id="contact" class="contact mb-5">
+        <div class="container" data-aos="fade-up">
+          <div class="row">
+            <div class="col-sm text-center mb-3">
+              <h2>New Post</h2>
+            </div>
+          </div>
+
+          <div class="row gy-4">
+            <div class="col-md-4">
+              <div class="info-item">
+                <i class="bi bi-sun-fill"></i>
+                <h3>CORE SELF</h3>
+                <address>Ego, personality, life path, surface desires, self-image.</address>
+              </div>
+            </div>
+            <!-- End Info Item -->
+
+            <div class="col-md-4">
+              <div class="info-item info-item-borders">
+                <i class="bi bi-moon-fill"></i>
+                <h3>INNER SELF</h3>
+                <p><a href="tel:+155895548855">Emotions, insecurities, fears, dreams.</a></p>
+              </div>
+            </div>
+            <!-- End Info Item -->
+
+            <div class="col-md-4">
+              <div class="info-item">
+                <i class="bi bi-arrow-up"></i>
+                <h3>OUTER SELF</h3>
+                <p><a href="mailto:info@example.com">External mask, intuition, moral code, life approach.</a></p>
+              </div>
+            </div>
+            <!-- End Info Item -->
+          </div>
+
+          <div class="form mt-5">
+            <form
+              action="forms/contact.php"
+              method="post"
+              role="form"
+              class="php-email-form"
+              v-on:submit.prevent="submit()"
+            >
+              <ul>
+                <li v-for="error in errors" v-bind:key="error">{{ error }}</li>
+              </ul>
+              <div class="row">
+                <div class="form-group col-md-6">
+                  <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                      <label class="mr-3">The Big 3:</label>
+                    </div>
+                    <select
+                      name="Sign Type"
+                      v-model="newPostParams.sign_type"
+                      class="form-select"
+                      aria-label=".form-select-sm example"
+                    >
+                      <option value="Sun">Sun</option>
+                      <option value="Moon">Moon</option>
+                      <option value="Rising">Rising</option>
+                    </select>
+                  </div>
+                </div>
+                <div class="form-group col-lg">
+                  <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                      <label class="mr-3">Sign:</label>
+                    </div>
+                    <select
+                      name="Sign"
+                      v-model="newPostParams.sign"
+                      class="form-select"
+                      aria-label=".form-select-sm example"
+                    >
+                      <option value="Aries">Aries</option>
+                      <option value="Taurus">Taurus</option>
+                      <option value="Gemini">Gemini</option>
+                      <option value="Cancer">Cancer</option>
+                      <option value="Leo">Leo</option>
+                      <option value="Virgo">Virgo</option>
+                      <option value="Libra">Libra</option>
+                      <option value="Scorpio">Scorpio</option>
+                      <option value="Sagittarius">Sagittarius</option>
+                      <option value="Capricorn">Capricorn</option>
+                      <option value="Aquarius">Aquarius</option>
+                      <option value="Pisces">Pisces</option>
+                    </select>
+                  </div>
+                </div>
+              </div>
+              <div class="form-group">
+                <textarea
+                  class="form-control"
+                  name="message"
+                  rows="5"
+                  placeholder="How are the planets aligning for you today?"
+                  v-model="newPostParams.post_content"
+                ></textarea>
+              </div>
+              <div class="my-3">
+                <div class="loading">Loading</div>
+                <div class="error-message"></div>
+                <div class="sent-message">Posted!</div>
+              </div>
+              <div class="text-center"><button type="submit" value="Create">Post</button></div>
+            </form>
+          </div>
+          <!-- End Contact Form -->
+        </div>
+      </section>
+    </main>
   </div>
 </template>
 
-<style></style>
+<style>
+.new-post {
+  padding-top: 0px;
+}
+</style>
