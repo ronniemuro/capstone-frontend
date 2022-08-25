53e151ec464855a49f731770305e4b8978150e92
Ronnie Lopez-Muro <ronniethan@outlook.com>
2022-08-25 16:35:47 -0600

    heroku

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from "./router";
 import axios from "axios";
 
 axios.defaults.baseURL =
-  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://synastry-backend.netlify.app/";
+  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://synastry-backend.herokuapp.com/";
 
 var jwt = localStorage.getItem("jwt");
 if (jwt) {
