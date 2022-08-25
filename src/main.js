278cc780d2bfd5c738781794f34022e5bac062cb
Ronnie Lopez-Muro <ronniethan@outlook.com>
2022-08-25 15:25:39 -0600

    changes development to netlify link

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from "./router";
 import axios from "axios";
 
 axios.defaults.baseURL =
-  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://synastry-backend.herokuapp.com/";
+  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://synastry-backend.netlify.app";
 
 var jwt = localStorage.getItem("jwt");
 if (jwt) {
